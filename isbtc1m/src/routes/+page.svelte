2c754097f7bc025f2d89421824fdfb3359c9f0f5
Raghav Sood <r@raghavsood.com>
2024-06-26 12:47:05 +0800

    all: add footer, improve twitter rendering

--- isbtc1m/src/routes/+page.svelte
+++ isbtc1m/src/routes/+page.svelte
@@ -34,18 +34,23 @@
 </script>
 
 <div class="flex flex-col items-center justify-center h-screen {backgroundClass} font-mono p-2">
-  <p class="text-white text-sm">
-    Is the price of Bitcoin over $1M?
-  </p>
-  <h1 class="text-white text-9xl font-bold">
-    {displayPrice ? priceOverMillion : '?'}
-  </h1>
-  <p class="text-white text-2xl mt-4">
-    {displayPrice ? displayPrice : 'Loading...'}
-  </p>
-  <a href="/why" class="text-xs text-white hover:underline hover:decoration-dashed mt-4">
-    Why?
-  </a>
+  <div class="flex flex-col items-center justify-center flex-grow">
+    <p class="text-white text-sm">
+      Is the price of Bitcoin over $1M?
+    </p>
+    <h1 class="text-white text-9xl font-bold">
+      {displayPrice ? priceOverMillion : '?'}
+    </h1>
+    <p class="text-white text-2xl mt-4">
+      {displayPrice ? displayPrice : 'Loading...'}
+    </p>
+    <a href="/why" class="text-xs text-white hover:underline hover:decoration-dashed mt-4">
+      Why?
+    </a>
+  </div>
+  <footer class="py-4 bg-transparent text-center text-white text-xs sticky bottom-0">
+    Built with <span class="text-red-500">&hearts;</span> by <a href="https://raghavsood.com" target="_BLANK" class="hover:underline">Raghav Sood</a>
+  </footer>
 </div>
 
 <style>
